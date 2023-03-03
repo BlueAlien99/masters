dd08c51c5bf35d05580c23b26a4f5162720dd491
Rafal Kulus <rkulus@sumologic.com>
2023-03-03 15:59:43 +0100

    sentence dataclass

--- src/helpers/data.py
+++ src/helpers/data.py
@@ -1,6 +1,7 @@
 import os
+from dataclasses import dataclass
 from enum import Enum
-from typing import Literal
+from typing import Literal, Optional
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -14,16 +15,42 @@ class Datasets(str, Enum):
 DataType = Literal['test', 'train']
 
 
+@dataclass
+class Sentence:
+    string: str
+    tokens: list[str]
+    chunks: Optional[list[list[int]]]
+
+    def __init__(self, sentence: str, tokens: list[str], chunks: Optional[list[list[int]]] = None):
+        self.string = sentence
+        self.tokens = tokens
+        self.chunks = chunks
+
+    @staticmethod
+    def from_sentence(sentence: str):
+        return Sentence(sentence, sentence.split())
+
+    @staticmethod
+    def from_chunks(chunks_str: str):
+        chunks = [chunk.strip()
+                  for chunk in chunks_str.strip()[1:-1].split('] [')]
+        sentence = ' '.join(chunks)
+        tokens = sentence.split()
+        chunks_id = [[tokens.index(token)
+                      for token in chunk.split()] for chunk in chunks]
+        return Sentence(sentence, tokens, chunks_id)
+
+
 def _get_data(datatype: DataType, dataset: Datasets, chunked: bool):
     filenames = [
         f'STSint.{"test" if datatype == "test" else ""}input.{dataset}.sent{i}{".chunk" if chunked else ""}.txt' for i
         in [1, 2]]
-    filepaths = list(map(lambda filename: f'{dir_path}/../../data/{filename}', filenames))
+    filepaths = [f'{dir_path}/../../data/{filename}' for filename in filenames]
 
     files_contents = []
     for filepath in filepaths:
         with open(filepath, 'r') as f:
-            file_content = list(map(lambda sent: sent.strip(), f.readlines()))
+            file_content = [sentence.strip() for sentence in f.readlines()]
             files_contents.append(file_content)
 
     return list(zip(*files_contents))
@@ -31,11 +58,13 @@ def _get_data(datatype: DataType, dataset: Datasets, chunked: bool):
 
 def get_data_gs(datatype: DataType, dataset: Datasets):
     data = _get_data(datatype, dataset, True)
-    data = list(map(lambda pair: list(
-        map(lambda sent: list(map(lambda chunk: chunk.strip(), sent.strip()[1:-1].split('] ['))), pair)), data))
+    data = [list(map(Sentence.from_chunks, pair)) for pair in data]
 
     return data
 
 
 def get_data_sys(datatype: DataType, dataset: Datasets):
-    return _get_data(datatype, dataset, False)
+    data = _get_data(datatype, dataset, False)
+    data = [list(map(Sentence.from_sentence, pair)) for pair in data]
+
+    return data
--- src/uwb.py
+++ src/uwb.py
@@ -1,4 +1,4 @@
-
+from helpers.data import get_data_gs, get_data_sys, Datasets
 
 
 
@@ -11,7 +11,8 @@ def process_sentence(pair):
 
 
 def main():
-    pass
+    print(get_data_sys('test', Datasets.H)[0])
+    print(get_data_gs('test', Datasets.H)[0])
 
 
 if __name__ == '__main__':
